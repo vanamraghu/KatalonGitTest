cb7a051ab42d9ef502be3ee350081093cebc540f
vanamraghu <raghavendravanam@10.0.1.17>
2018-06-05 21:19:32 +0530

    Code about Testing google api

---
+++ Object Repository/GoogleAPIbook.rs
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebServiceRequestEntity>
+   <description>Google api about books</description>
+   <name>GoogleAPIbook</name>
+   <tag></tag>
+   <elementGuidId>04be8d24-056b-4d37-bbdd-280535a5c814</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <httpBody></httpBody>
+   <httpBodyContent></httpBodyContent>
+   <httpBodyType></httpBodyType>
+   <migratedVersion>5.4.1</migratedVersion>
+   <restRequestMethod>GET</restRequestMethod>
+   <restUrl>https://www.googleapis.com/books/v1/volumes?q=harry+potter</restUrl>
+   <serviceType>RESTful</serviceType>
+   <soapBody></soapBody>
+   <soapHeader></soapHeader>
+   <soapRequestMethod></soapRequestMethod>
+   <soapServiceFunction></soapServiceFunction>
+   <wsdlAddress></wsdlAddress>
+</WebServiceRequestEntity>
